d282d8a3aea604a76e2d130585241c872178634a
mohammed sabeel <sabeel@sellowpay.com>
2024-02-20 16:16:26 +0530

    added postgress database

--- django_docker/settings.py
+++ django_docker/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'django_docker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres-django",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
